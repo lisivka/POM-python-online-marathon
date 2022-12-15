459a5a19cc047dba1f0f126bdcbdb6c9ed878687
LSM <lisivka@gmail.com>
2022-12-15 23:45:30 +0200

    add Task 3

---
+++ Sprint 01/task_3.py
@@ -0,0 +1,12 @@
+"""Given a string, check if its characters can be rearranged to form a palindrome.
+Where a palindrome is a string that reads the same left-to-right and right-to-left.
+
+Example
+"trueistrue" -> false;
+"abcab" -> true because "abcba" is a palindrome
+[input] string s (min 1 letters)
+
+[output] boolean"""
+
+def isPalindrome(str):
+    pass
