a5acf55f20e4482a4b28001b1237442e87e57a62
Lisivka <lisivka@gmail.com>
2022-12-16 13:41:15 +0200

    add Task 4

---
+++ Sprint 01/task_4.py
@@ -0,0 +1,15 @@
+"""
+Given two permutations p and q of length n. Find a permutation r, such that for every 1 <= i <= n, q[i] = p[r[i]].
+Permutation of length n is an array consisting of distinct numbers from 1 to n in some order.
+
+Example
+Input:
+p = [5, 1, 3],  q = [3, 1, 5]
+
+Output:
+r = [3, 2, 1]
+[input] array.integer p
+[input] array.integer q
+[output] array.integer
+permutation r
+"""
