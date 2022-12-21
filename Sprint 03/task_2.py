bfe127237be3e44e49c83c3afbaa4f71d6937999
LSM <lisivka@gmail.com>
2022-12-21 21:15:00 +0200

    Spint 03 - Task 2,4,6 Done

--- Sprint 03/task_2.py
+++ Sprint 03/task_2.py
@@ -1,10 +1,22 @@
-"""
-Create function create with one string argument.
-This function should return anonymous function that checks
-if the argument of function is equals to the argument of outer function.
+"""Create function create with one string argument.
+This function should return anonymous function
+that checks if the argument of function
+is equals to the argument of outer function.
+
+
 
 For example:
  tom = create("pass_for_Tom")
  tom("pass_for_Tom") returns true
  tom("pass_for_tom") returns false
 """
+
+
+def create(s):
+    return lambda x: x == s
+
+
+if __name__ == "__main__":
+    tom = create("pass_for_Tom")
+    print(tom("pass_for_Tom"))
+    print(tom("pass_for_tom"))
