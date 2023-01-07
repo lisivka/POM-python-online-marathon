b5b2a374c5a0460a5f9480567e7c0dca67bd8a01
LSM <lisivka@gmail.com>
2023-01-07 16:08:13 +0200

    Sprint-06 Task 4 in progress

--- Sprint 06 With (in, out). Serialization/task_4.py
+++ Sprint 06 With (in, out). Serialization/task_4.py
@@ -18,17 +18,31 @@ from json import JSONEncoder
 
 
 class Student:
+    def __init__(self, full_name: str, avg_rank: float, courses: list):
+        self.full_name = full_name
+        self.avg_rank = avg_rank
+        self.courses = courses
 
     @classmethod
     def from_json(cls, json_file):
-        pass
+        folder = "Test_data/"
+        with open(folder +json_file) as f:
+            data = json.load(f)
+            values = list(data.values())
+            return cls(*values)
 
     @staticmethod
     def serialize_to_json(cls, filename):
-        pass
-
+        with open(filename, "w") as fw:
+            # print(cls.__dict__)
+            json.dump(cls.__dict__, fw,indent=None)
+    def __str__(self):
+        return f"{self.full_name} ({self.avg_rank}): {self.courses}"
 
 class Group:
+    def __init__(self, title: str, students: list):
+        self.title = title
+        self.students = students
 
     @classmethod
     def serialize_to_json(cls, list_of_groups, filename):
@@ -36,6 +50,7 @@ class Group:
 
     @classmethod
     def create_group_from_file(cld, students_file):
+
         pass
 
 
@@ -47,7 +62,22 @@ if __name__ == "__main__":
 
     ## ========================================
 
-    user1 = Student.from_json("2020-01.json")
-    Student.serialize_to_json(user1, "test_student.json")
-    # print_file("test_student.json")
-    # {"full_name": "Student2 from group2", "avg_rank": 50.4, "courses": ["C++"]}
+    # user1 = Student.from_json("2020-01.json")
+    # Student.serialize_to_json(user1, "test_student.json")
+    # # print_file("test_student.json")
+    # # {"full_name": "Student2 from group2", "avg_rank": 50.4, "courses": ["C++"]}
+
+    ## ========================================
+    #
+    # import json
+    # folder ="Test_data/"
+    # with open(folder + "2020_2.json") as read_file:
+    #     user2 = json.load(read_file)
+    # print([str(Student(**item)) for item in user2])
+
+    ## ========================================
+
+    g1 = Group.create_group_from_file("2020_2.json")
+    g2 = Group.create_group_from_file("2020-01.json")
+    Group.serialize_to_json([g1, g2], "g1")
+    # print_file("g1")
