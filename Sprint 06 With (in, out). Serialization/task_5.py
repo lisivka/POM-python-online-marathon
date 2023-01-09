00d69b20c1e3872b2623d75a61e505fb933215ce
Lisivka <lisivka@gmail.com>
2023-01-09 15:03:09 +0200

    Sprint-6 Task 5 in progress

--- Sprint 06 With (in, out). Serialization/task_5.py
+++ Sprint 06 With (in, out). Serialization/task_5.py
@@ -12,6 +12,9 @@ serialize(user_dict, "2", FileType.BYTE) -> creates file with name "2"
 and this file will contain user_dict as byte array
 serialize("String", "string.json", FileType.JSON) -> creates file with name "string.json" and text "String"
 '''
+# https://stackoverflow.com/questions/1984325/explaining-pythons-enter-and-exit
+# https://docs-python.ru/standart-library/modul-enum-perechislenija-python/
+# https://pavel-karateev.gitbook.io/intermediate-python/sintaksis/context_managers
 
 import json
 import pickle
@@ -23,32 +26,41 @@ class SerializeManager:
     def __init__(self, file_name, file_type):
         self.file_name = file_name
         self.file_type = file_type
+        # self.file = None
 
+    def __enter__(self):
+        print("__enter__")
+        self.file = open(self.file_name, 'w')
+        return self
 
-    def serialize(self, file_name, file_type):
+    def serialize(self, object):
+        print("_serialize_")
+        json.dump(object, self.file)
         pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("__exit__")
+        self.file.close()
 
 
 class FileType(Enum):
-    JSON = 1
-    def __int__(self):
-        pass
+    JSON = "json"
 
-def serialize(object, filename, filetype):
-    # print(object, filename, filetype)
-    # print(SerializeManager(filename, filetype))
-    with SerializeManager(filename, filetype) as manager:
+
+def serialize(object, filename, fileType):
+    with SerializeManager(filename, fileType) as manager:
         manager.serialize(object)
 
 
+
 if __name__ == "__main__":
-    print(isinstance(serialize.__globals__['SerializeManager'], object))
-    # True
-    print(issubclass(FileType, Enum))
-    # True
+    # print(isinstance(serialize.__globals__['SerializeManager'], object))
+    # # True
+    # print(issubclass(FileType, Enum))
+    # # True
 
     from os import path
 
     # print(str(path.exists('1')))
-    serialize("String", "1", FileType.JSON)
+    serialize("String", "1.json", FileType.JSON)
     print(str(path.exists('1')))
