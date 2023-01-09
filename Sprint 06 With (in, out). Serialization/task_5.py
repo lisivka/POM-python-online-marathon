d917891bbc56a48fdbf96c37802e3455204f10f0
Lisivka <lisivka@gmail.com>
2023-01-09 12:25:08 +0200

    Sprint-6 Task 5 in progress

--- Sprint 06 With (in, out). Serialization/task_5.py
+++ Sprint 06 With (in, out). Serialization/task_5.py
@@ -1,5 +1,6 @@
 '''
-Create context manager class SerializeManager with attributes filename and type for serializing python object to different formats.
+Create context manager class SerializeManager with attributes filename
+and type for serializing python object to different formats.
 This class should contain method serialize for serialize object to filename according to  type.
 For defining format create enum FileType with values JSON, BYTE.
 Create function serialize(object, filename, filetype).
@@ -7,7 +8,8 @@ This function use SerializeManager and should serialize object to filename accor
 For example:
 if user_dict = { 'name': 'Roman', 'id': 8}
 then
-serialize(user_dict, "2", FileType.BYTE) -> creates file with name "2" and this file will contain user_dict as byte array
+serialize(user_dict, "2", FileType.BYTE) -> creates file with name "2"
+and this file will contain user_dict as byte array
 serialize("String", "string.json", FileType.JSON) -> creates file with name "string.json" and text "String"
 '''
 
@@ -15,12 +17,38 @@ import json
 import pickle
 from enum import Enum
 
-# // type your code here
 
-def serialize(object, filename, fileType):
-    with SerializeManager(filename, fileType) as manager:
+# // type your code here
+class SerializeManager:
+    def __init__(self, file_name, file_type):
+        self.file_name = file_name
+        self.file_type = file_type
+
+
+    def serialize(self, file_name, file_type):
+        pass
+
+
+class FileType(Enum):
+    JSON = 1
+    def __int__(self):
+        pass
+
+def serialize(object, filename, filetype):
+    # print(object, filename, filetype)
+    # print(SerializeManager(filename, filetype))
+    with SerializeManager(filename, filetype) as manager:
         manager.serialize(object)
 
 
-# print(isinstance(serialize.__globals__['SerializeManager'], object))
-# True
+if __name__ == "__main__":
+    print(isinstance(serialize.__globals__['SerializeManager'], object))
+    # True
+    print(issubclass(FileType, Enum))
+    # True
+
+    from os import path
+
+    # print(str(path.exists('1')))
+    serialize("String", "1", FileType.JSON)
+    print(str(path.exists('1')))
